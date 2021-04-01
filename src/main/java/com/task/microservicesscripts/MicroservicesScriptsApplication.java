55d884845328cfa862c7dc0675fc5e5e9f23c02a
HeatLee <32158GitDth>
2021-04-01 17:49:26 +0300

    Project creation

---
+++ src/main/java/com/task/microservicesscripts/MicroservicesScriptsApplication.java
@@ -0,0 +1,13 @@
+package com.task.microservicesscripts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesScriptsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroservicesScriptsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/task/microservicesscripts/MicroservicesScriptsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.task.microservicesscripts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesScriptsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
